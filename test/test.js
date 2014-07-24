0c09cc7e367b4eb32c219953f8142eaf8b97daae
Aleksey Smolenchuk <aleksey@uber.com>
2014-07-24 16:06:22 -0700

    add tests

---
+++ test/test.js
@@ -0,0 +1,35 @@
+require('../');
+require('should');
+var http = require('http');
+var https = require('https');
+
+describe('http.globalAgent', function () {
+  it('should have infinite maxSockets', function () {
+    http.globalAgent.maxSockets.should.equal(Infinity);
+  });
+});
+
+describe('https.globalAgent', function () {
+  it('should have infinite maxSockets', function () {
+    http.globalAgent.maxSockets.should.equal(Infinity);
+  });
+});
+
+describe('an instance of http.Agent', function () {
+  it('should have infinite maxSockets', function () {
+    (new http.Agent()).maxSockets.should.equal(Infinity);
+  });
+});
+
+describe('an instance of https.Agent', function () {
+  it('should have infinite maxSockets', function () {
+    (new https.Agent()).maxSockets.should.equal(Infinity);
+  });
+});
+
+
+
+
+
+
+
